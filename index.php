acdaa672031bceaa7c3f133486f8af7ec74860cd
gabi2102-code <gabriela_b_ferreira@estudante.sesisenai.org.br>
2026-08-17 09:47:55 -0300

    add- index

--- index.php
+++ index.php
@@ -1,7 +1,74 @@
 <?php
 
 include "infra/conexao.php";
+$livros = mysqli_query($conexao, "SELECT * FROM clientes");
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante </title>
+    <link rel="stylesheet" href="style/styles.css">
+</head>
+
+<body>
+    <header>
+        <h1> Restaurante </h1>
+    </header>
+    <main>
+        <h2>Adicione um novo prato!</h2>
+        <form action="public/cadastrar.php" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome">
+            <br>
+            <label for="descricao">Descrição:</label>
+            <input type="text" name="descricao">
+            <br>
+            <label for="preco">Preço:</label>
+            <input type="number" name="preco">
+            <br>
+            <label for="categoria">Categoria:</label>
+            <input type="text" name="categoria">
+            <br>
+            <button type="submit">Cadastrar</button>
+        </form>
+        <div>
+            <h2>Pratos cadastrado</h2>
+            <table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Preço</th>
+                    <th>Categoria</th>
+                </tr>
+                <?php while ($pratos = mysqli_fetch_assoc($pratos)) { ?>
+                    <tr>
+                        <td><?php echo $pratos["nome"] ?></td>
+                        <td><?php echo $pratos["descrição"] ?></td>
+                        <td><?php echo $pratos["preço"] ?></td>
+                        <td><?php echo $pratos["categoria"] ?></td>
+                        <td>
+                            <a href="public/editar.php?id=<?php echo $pratos["id"] ?>">Editar</a>
+                            <a href="public/excluir.php?id=<?php echo $pratos["id"] ?>">Excluir</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+
+    </main>
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html>
 
 
 
