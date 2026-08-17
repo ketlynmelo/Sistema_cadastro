e6fb39dd8066f3d621258f0c8e6e0bfbd3cddd42
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-08-17 08:35:51 -0300

    ADD - Adicionei as tabelas e informacoes no sql

---
+++ database/db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE restaurante;
+USE restaurante;
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+    
+    );
+
+CREATE TABLE pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
+    preco FLOAT NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    id_cliente INT NOT NULL,
+    CONSTRAINT fk_pedidos_clientes FOREIGN KEY (id_cliente) REFERENCES clientes(id)
+
+
+);
